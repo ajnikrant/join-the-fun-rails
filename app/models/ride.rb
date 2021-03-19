6e08023d0ad4158a53f725d3708d605d2b387abb
Amanda <ajnikrant@gmail.com>
2021-03-18 20:36:02 -0400

    commit tree ab63724deb006590318fa8024bb1584d4ead3cad

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,8 @@
 class Ride < ActiveRecord::Base
+    belongs_to :taxi
+    belongs_to :passenger
 end
+
+rails generate migration add_fieldname_to_tablename fieldname:string.
+rails generate migration add_passenger_id_to_rides passenger_id:integer
+rails generate migration add_taxi_id_to_rides taxi_id:integer
